46f794813f70f43ab17ce5ff05f33abe36031af3
Aria <ariatehrani84@gmail.com>
2026-08-01 18:51:45 +0330

    fix some tests and delete non-usage codes

--- tests/test_semantic_errors.c
+++ tests/test_semantic_errors.c
@@ -1,25 +1,19 @@
 int outer_var = 10;
 
-// خطا: پارامترهای تکراری (param1)
 int calculate(int param1, int param1) {
-    // هشدار: سایه‌اندازی (Shadowing) روی متغیر outer_var
     int outer_var = 20;
 
     int x;
-    // هشدار: استفاده از x قبل از مقداردهی
     int y = x;
 
     int z = 5;
-    // خطا: تعریف مجدد متغیر z در همان دامنه
     int z = 10;
 
-    // خطا: متغیر تعریف نشده
     missing_var = 100;
 
     return 0;
 }
 
-// خطا: تعریف مجدد یک تابع با همان نام
 int calculate(int a) {
     return a;
 }
--- tests/test_semantic_types.c
+++ tests/test_semantic_types.c
@@ -1,22 +1,15 @@
-// تابعی که یک عدد صحیح و یک اعشاری می‌گیرد
 int process_data(int a, float b) {
-    // نقطه کور 1: تلاش برای برگرداندن یک رشته در تابعی که خروجی‌اش int است
     return "This is a string, not an int!";
 }
 
 void test_type_system() {
-    // نقطه کور 2: متغیر از نوع int است، اما رشته به آن داده شده
     int invalid_assign = "Hello World";
 
-    // نقطه کور 3: مقداردهی bool به int (باید خطا بدهد چون توابع ریاضی و منطقی در تایپ‌چکر ما جدا هستند)
     int boolean_to_int = true;
 
-    // کاملا درست (تطابق int و float مجاز است)
     float valid_assign = 10;
 
-    // نقطه کور 4: فراخوانی تابع با تعداد آرگومان اشتباه
     process_data(5);
 
-    // نقطه کور 5: فراخوانی تابع با تعداد درست، اما نوع اشتباه (رشته به جای عدد)
     process_data("Not a number", 3.14);
 }
